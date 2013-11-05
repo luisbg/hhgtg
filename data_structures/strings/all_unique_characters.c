afcb18b106fbe09e2d384beeec4b8fb348ea034d
Luis de Bethencourt <luis@debethencourt.com>
2013-11-04 20:13:47 -0500

    strings: all unique chars

---
+++ data_structures/strings/all_unique_characters.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+typedef enum
+{FALSE, TRUE} bool;
+
+bool all_unique_characters (char * str) {
+  if (str == NULL)
+    return TRUE;
+
+  int c;
+  int n;
+  for (c = 0; str[c] != 0; c++) {
+    for (n = c + 1; str[n] != 0; n++) {
+      if (str[c] == str[n]) {
+        return FALSE;
+      }
+    }
+  }
+
+  return TRUE;
+}
+
+int main ()
+{
+  char * test = "";
+  printf("%s :: unique characters: %s\n", test,
+    all_unique_characters(test)? "yes": "no");
+  test = "hello";
+  printf("%s :: unique characters: %s\n", test,
+    all_unique_characters(test)? "yes": "no");
+  test = "world";
+  printf("%s :: unique characters: %s\n", test,
+    all_unique_characters(test)? "yes": "no");
+}
