3c8f7d2937bd4c614a40d615f45ac5ae1edc3ef5
Luis de Bethencourt <luis@debethencourt.com>
2013-11-06 17:12:23 -0500

    strings: compress repeated characters

---
+++ data_structures/arrays/rotate_90_degrees.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void show_array (int size, int arr[size][size]) {
+  int x, y;
+  for (x = 0; x < size; x++) {
+    for (y = 0; y < size; y++) {
+      printf("%d ", arr[x][y]);
+    }
+    printf("\n");
+  }
+}
+
+void rotate_90_degrees (int size, int arr[size][size], int new[size][size]) {
+  new = malloc (size * size * sizeof(int));
+
+  int x, y;
+  int new_y;
+  for (x = 0; x < size; x++) {
+    for (y = 0; y < size; y++) {
+      if (x <= size)
+        new_y = (size-1) - x;
+      else
+        new_y = x - (size-1);
+
+      new[x][y] = arr[y][new_y];
+    }
+  }
+
+  printf("\n");
+  show_array(4, new);
+  printf("\n");
+}
+
+
+int main () {
+  int arr[4][4] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+  int rot[4][4];
+
+  show_array(4, arr);
+
+  rotate_90_degrees (4, arr, rot);
+  show_array(4, rot);
+}
---
+++ data_structures/strings/compression_repeated_characters.c
@@ -0,0 +1,62 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ASCII_SIZE 256
+
+typedef enum
+{ FALSE, TRUE } bool;
+
+char * compress_string (char * str, int len) {
+  int chars[ASCII_SIZE] = { 0 };
+
+  int c;
+  int nss = 0;
+  for (c = 0; c < len; c++) {
+    if (chars[str[c]] == 0) {   /* a new character */
+      nss += 2;              /* needs to spaces in the compressed str */
+    }
+
+    chars[str[c]]++;
+
+    if (chars[str[c]] >= 10) /* one more if a two digit number */
+      nss++;
+  }
+
+  printf("%d %d\n", nss, len);
+  if (nss < len ) {
+    char tmp;
+    char * new = (char *) malloc (nss * sizeof(char)); 
+
+    int pos = 0;
+    for (c = 0; c < ASCII_SIZE; c++) {
+      if (chars[c] > 0) {
+        new[pos++] = c;
+        if (chars[c] < 10) {
+          sprintf(&tmp, "%d", chars[c]);
+          new[pos++] = tmp;
+        } else {
+          sprintf(&tmp, "%d", chars[c] / 10);
+          new[pos++] = tmp;
+          sprintf(&tmp, "%d", chars[c] % 10);
+          new[pos++] = tmp;
+        }
+      }
+    }
+    new[pos] = '\0';
+
+    return new;
+  } else {
+    return str;
+  }
+}
+
+
+int main () {
+  char * test = "aabbbcddeeeeeeeeeeee";
+  printf("%s :: compressed :: %s\n", test, compress_string(test, strlen(test)));  
+
+  test = "abbccdde";
+  printf("%s :: compressed :: %s\n", test, compress_string(test, strlen(test)));  
+
+}
