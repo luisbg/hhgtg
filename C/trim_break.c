f8d951280412ec3168e808e9fd71b1e242b70a40
Luis de Bethencourt <luis@debethencourt.com>
2013-11-11 17:37:43 -0500

    C: trim break

---
+++ C/trim_break.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/* trim: remove trailing blanks, tabs, newlines */
+int trim(char s[])
+{
+  int n;
+  for (n = strlen(s)-1; n >= 0; n--)
+    if (s[n] != ' ' && s[n] != '\t' && s[n] != '\n')
+      break;
+
+  s[n+1] = '\0';
+  return n;
+}
+
+main ()
+{
+  char s[11] = "test\n \n ";
+  trim(s);
+  printf("%s.\n", s);
+}
