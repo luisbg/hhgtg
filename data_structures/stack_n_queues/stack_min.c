db7ac7cc652dfcc2cbc0d2d1330f1bfd54ef1266
Luis de Bethencourt <luis@debethencourt.com>
2013-11-13 13:17:02 -0500

    stack: min in 0(1)

---
+++ data_structures/stack_n_queues/stack_min.c
@@ -0,0 +1,106 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node
+{
+  int data;
+  struct node *next;
+} node;
+
+typedef struct stack
+{
+  node *top;
+  node *min;
+} stack;
+
+void push (stack * s, int data);
+int pop (stack * s);
+int peek (stack s);
+int min (stack s);
+
+void
+push (stack * s, int data)
+{
+  node *new = (node *) malloc (sizeof (node));
+  new->data = data;
+
+  new->next = s->top;
+  s->top = new;
+
+  if (data < min (*s)) {
+    node *new_min = (node *) malloc (sizeof (node));
+    new_min->data = data;
+    new_min->next = s->min;
+    s->min = new_min;
+  }
+}
+
+int
+pop (stack * s)
+{
+  int value = -1;
+  node *tmp = NULL;
+
+  if (s->top != NULL) {
+    value = s->top->data;
+    tmp = s->top;
+    s->top = s->top->next;
+    free (tmp);
+  }
+
+  if (value == min (*s)) {
+    tmp = s->min;
+    s->min = s->min->next;
+    free (tmp);
+  }
+
+  return value;
+}
+
+int
+min (stack s)
+{
+  if (s.min == NULL) {
+    return 10000;
+  } else {
+    return s.min->data;
+  }
+}
+
+int
+peek (stack s)
+{
+  if (s.top != NULL)
+    return s.top->data;
+  else
+    return -1;
+}
+
+int
+main ()
+{
+  stack s;
+  printf ("push: %d\n", 2);
+  push (&s, 2);
+  printf ("push: %d\n", 3);
+  push (&s, 3);
+  printf ("push: %d\n", 1);
+  push (&s, 1);
+  printf ("\n");
+
+  printf ("min: %d\n", min (s));
+  printf ("pop: %d\n", pop (&s));
+  printf ("min: %d\n", min (s));
+  printf ("pop: %d\n", pop (&s));
+  printf ("min: %d\n", min (s));
+  printf ("\n");
+
+  printf ("push: %d\n", 4);
+  push (&s, 4);
+  printf ("push: %d\n", 5);
+  push (&s, 5);
+  printf ("\n");
+
+  printf ("pop: %d\n", pop (&s));
+  printf ("min: %d\n", min (s));
+}
