98ac20cae7937b85d5dee48e0205b2666f41446c
Luis de Bethencourt <luis@debethencourt.com>
2013-11-27 22:41:31 -0500

    strings: remove characters in place

---
+++ data_structures/strings/remove_characters.c
@@ -0,0 +1,89 @@
+/* Write an efficient function that deletes characters from a string.
+ * Use the prototype
+ * 
+ * string removeChars( string str, string remove );
+ * 
+ * where any character existing in remove must be deleted from str. For
+ * example, given a str of “Battle of the Vowels: Hawaii vs. Grozny” and a
+ * remove of “aeiou”, the function should transform str to “Bttl f th Vwls:
+ * Hw vs. Grzny”. Justify any design decisions you make and discuss the
+ * efficiency of your solution.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum
+{
+  FALSE, TRUE
+} bool;
+
+
+void remove_chars (char **str, char *remove);
+bool set_char_map (char *remove, bool ** map);
+
+
+void
+remove_chars (char **str, char *remove)
+{
+  int c, r, pos;
+  bool *map;
+  set_char_map (remove, &map);
+
+  pos = 0;
+  for (c = 0; (*str)[c] != '\0'; c++) {
+    if (!map[(*str)[c]]) {
+      if (pos != c) {
+        (*str)[pos] = (*str)[c];
+      }
+      pos++;
+    }
+  }
+  (*str)[pos] = '\0';
+
+  return;
+}
+
+/* Would be better to use a Hash Table to save memory. Specially so unicode
+ * can be supported. */
+bool
+set_char_map (char *remove, bool ** map)
+{
+  bool *tmp = (bool *) malloc (256 * sizeof (bool));
+  if (!tmp)
+    return FALSE;
+  int c;
+
+  for (c = 0; c < 256; c++) {
+    tmp[c] = FALSE;
+  }
+
+  for (c = 0; remove[c] != '\0'; c++) {
+    tmp[remove[c]] = TRUE;
+  }
+
+  *map = tmp;
+
+  return TRUE;
+}
+
+
+int
+main ()
+{
+  // original has to be dynamic so it can be changed in place
+  char *original = (char *) malloc (60 * sizeof (char));
+  char *tmp = "Battle of the Vowels: Hawaii vs. Grozny";
+  int c;
+  for (c = 0; tmp[c] != '\0'; c++) {
+    original[c] = tmp[c];
+  }
+
+  char *remove = "aeoiu";
+
+  printf ("original: %s\n", original);
+  remove_chars (&original, remove);
+  printf ("removed:  %s\n", original);
+
+  return 0;
+}
