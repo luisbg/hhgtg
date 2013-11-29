c049a6a3abdcb76a72b85f20564e3e4a5e23fbbd
Luis de Bethencourt <luis@debethencourt.com>
2013-11-29 18:08:50 -0500

    C: Endianness

---
+++ C/endianness.c
@@ -0,0 +1,32 @@
+/* Returns true if the machine is little-endian, false if the
+ * machine is big-endian
+ */
+
+#include <stdio.h>
+
+typedef enum
+{
+  FALSE, TRUE
+} bool;
+
+
+bool
+endianness ()
+{
+  union {
+    int num;
+    char single_byte;
+  } test;
+
+  test.num = 1;
+  return test.single_byte;                /* Returns the byte at the lowest address */
+}
+
+
+int
+main ()
+{
+  printf ("Machine is %s Endian\n", endianness ()? "Little" : "Big");
+
+  return 0;
+}
