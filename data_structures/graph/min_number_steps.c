10a091b581b1d70e47e62c62caa2dea0a2a6f5ff
Luis de Bethencourt <luis@debethencourt.com>
2013-12-12 16:46:48 -0500

    graph: show shortest route between points

--- data_structures/graph/min_number_steps.c
+++ data_structures/graph/min_number_steps.c
@@ -7,6 +7,8 @@ typedef struct node
 {
   int x, y;
   int steps;                    // Number of steps we have taken to reach this point
+  int x_route[256];
+  int y_route[256];
 } node;
 
 typedef struct queue_node
@@ -26,12 +28,17 @@ typedef enum
   FALSE, TRUE
 } bool;
 
+
+node *min_steps (int w, int h, bool grid[w][h], int start_x, int start_y,
+    int end_x, int end_y);
+void show_steps (int w, int h, bool map[w][h], node * steps);
 static void init_queue (queue * q);
-static void enqueue (queue * q, int x, int y, int steps);
+static void enqueue (queue * q, int x, int y, int steps, node * prev);
 static node *dequeue (queue * q);
 static bool empty_queue (queue q);
 
-int
+
+node *
 min_steps (int w, int h, bool grid[w][h], int start_x, int start_y,
     int end_x, int end_y)
 {
@@ -50,7 +57,7 @@ min_steps (int w, int h, bool grid[w][h], int start_x, int start_y,
   init_queue (&q);
 
   // enqueue starting position with 0 steps
-  enqueue (&q, start_x, start_y, 0);
+  enqueue (&q, start_x, start_y, 0, NULL);
 
   while (!empty_queue (q)) {
     top = dequeue (&q);
@@ -72,7 +79,7 @@ min_steps (int w, int h, bool grid[w][h], int start_x, int start_y,
     visited[top->x][top->y] = TRUE;
 
     if (top->x == end_x && top->y == end_y)
-      return top->steps;
+      return top;
 
     // Generate all of the transitions between nodes
     // Generate the following deltas: (-1,-1), (-1,0), (-1,1), (0,-1), (0,0),
@@ -80,14 +87,50 @@ min_steps (int w, int h, bool grid[w][h], int start_x, int start_y,
     for (x_delta = -1; x_delta <= 1; x_delta++) {
       for (y_delta = -1; y_delta <= 1; y_delta++) {
         // Add the new node into the queue
-        enqueue (&q, top->x + x_delta, top->y + y_delta, top->steps + 1);
+        enqueue (&q, top->x + x_delta, top->y + y_delta, top->steps + 1, top);
       }
     }
 
     free (top);
   }
 
-  return -1;
+  return NULL;
+}
+
+void
+show_steps (int w, int h, bool map[w][h], node * steps)
+{
+  int c, r, s;
+  int route_map[w][h];
+
+  // Generate map with obstacles
+  for (r = 0; r < h; r++) {
+    for (c = 0; c < w; c++) {
+      if (map[c][r])
+        route_map[c][r] = 0;
+      else
+        route_map[c][r] = 1;
+    }
+  }
+
+  // Mark nodes which path used
+  for (s = 0; s < steps->steps; s++) {
+    route_map[steps->x_route[s]][steps->y_route[s]] = 2;
+  }
+
+  // Print map
+  printf ("\n");
+  for (r = 0; r < h; r++) {
+    for (c = 0; c < w; c++) {
+      if (route_map[c][r] == 0)
+        printf (". ");
+      else if (route_map[c][r] == 1)
+        printf ("x ");
+      else
+        printf ("O ");
+    }
+    printf ("\n");
+  }
 }
 
 static void
@@ -98,8 +141,10 @@ init_queue (queue * q)
 }
 
 static void
-enqueue (queue * q, int x, int y, int steps)
+enqueue (queue * q, int x, int y, int steps, node * prev)
 {
+  int i;
+
   queue_node *new_node = (queue_node *) malloc (sizeof (queue_node));
   node *pos = (node *) malloc (sizeof (node));
   pos->x = x;
@@ -107,6 +152,15 @@ enqueue (queue * q, int x, int y, int steps)
   pos->steps = steps;
   new_node->data = pos;
   new_node->next = NULL;
+
+  if (prev) {
+    for (i = 0; i < steps; i++) {
+      pos->x_route[i] = prev->x_route[i];
+      pos->y_route[i] = prev->y_route[i];
+    }
+    pos->x_route[steps] = x;
+    pos->y_route[steps] = y;
+  }
 
   if (q->head) {
     q->last->next = new_node;
@@ -166,14 +220,20 @@ main ()
   int h = 20;
   int start_x = 0, start_y = 0;
   int end_x = 29, end_y = 19;
-  int steps;
   bool map[w][h];
+  node *steps;
 
   init_map (w, h, map);
 
   steps = min_steps (w, h, map, start_x, start_y, end_x, end_y);
-  printf ("min number of steps between %d,%d and %d,%d: %d\n",
-      start_x, start_y, end_x, end_y, steps);
+  if (steps) {
+    printf ("min number of steps between %d,%d and %d,%d: %d\n",
+        start_x, start_y, end_x, end_y, steps->steps);
+
+    show_steps (w, h, map, steps);
+  } else
+    printf ("%d,%d can't be reached from %d,%d\n", end_x, end_y,
+        start_x, start_y);
 
   return 0;
 }
