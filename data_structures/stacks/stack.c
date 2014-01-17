d0a2247a1887ab89b48ae73397bc9c3f1051640e
Luis de Bethencourt <luis@debethencourt.com>
2014-01-16 22:04:54 -0500

    stacks: empty check and init

--- data_structures/stacks/stack.c
+++ data_structures/stacks/stack.c
@@ -12,9 +12,16 @@ typedef struct stack
   node *top;
 } stack;
 
+typedef enum
+{ FALSE, TRUE } bool;
+
+
 int pop (stack * s);
 void push (stack * s, int data);
 int peek (stack s);
+bool empty (stack s);
+stack init ();
+
 
 void
 push (stack * s, int data)
@@ -51,24 +58,52 @@ peek (stack s)
     return -1;
 }
 
+bool
+empty (stack s)
+{
+  if (s.top)
+    return FALSE;
+  else
+    return TRUE;
+}
+
+stack
+init ()
+{
+  stack * s = (stack *) malloc (sizeof (stack));
+  s->top = NULL;
+
+  return *s;
+}
+
+
 int
 main ()
 {
-  stack s;
+  stack s = init();
+  s.top = NULL;
+
   printf ("push: %d\n", 1);
   push (&s, 1);
   printf ("push: %d\n", 2);
   push (&s, 2);
-  printf ("push: %d\n", 3);
+  printf ("push: %d\n\n", 3);
   push (&s, 3);
+
+  printf ("empty stack? %s\n\n", empty (s)? "yes": "no");
 
   printf ("pop: %d\n", pop (&s));
 
   printf ("push: %d\n", 4);
   push (&s, 4);
-  printf ("push: %d\n", 5);
+  printf ("push: %d\n\n", 5);
   push (&s, 5);
 
-  printf ("peek: %d\n", peek (s));
+  printf ("peek: %d\n\n", peek (s));
   printf ("pop: %d\n", pop (&s));
+  printf ("pop: %d\n", pop (&s));
+  printf ("pop: %d\n", pop (&s));
+  printf ("pop: %d\n\n", pop (&s));
+
+  printf ("empty stack? %s\n", empty (s)? "yes": "no");
 }
