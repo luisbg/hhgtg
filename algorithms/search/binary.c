b932d1056895ab12c707f946e7abd3b63d614bf7
Luis de Bethencourt <luis@debethencourt.com>
2014-01-30 10:09:34 -0500

    search: more error checks in binary search

--- algorithms/search/binary.c
+++ algorithms/search/binary.c
@@ -1,53 +1,75 @@
+/* Recursive Binary Search */
+
+
 #include <stdio.h>
+
+
+static int middle (int *list, int key, int low, int high);
+static int interpolation_search (int *list, int key, int low, int high);
+int search (int *list, int key, int low, int high);
 
 
 static int step = 0;
 
 
-int middle (int *l, int key, int low, int high)
+static int
+middle (int *list, int key, int low, int high)
 {
   return (low + high) / 2;
 }
 
-int interpolation_search (int *l, int key, int low, int high)
+static int
+interpolation_search (int *list, int key, int low, int high)
 {
-  return low + (key - l[low]) * (high-low) / (l[high] - l[low]);
+  return low + (key - list[low]) * (high - low) / (list[high] - list[low]);
 }
 
-int search (int *l, int key, int low, int high)
+int
+search (int *list, int key, int low, int high)
 {
-  // printf ("step: %d\n", step++);
+  // printf ("step: %d :: %d %d\n", step++, low, high);
 
-  // int p = middle (l, key, low, high);
-  int p = interpolation_search (l, key, low, high);
+  // int p = middle (list, key, low, high);
+  int p = interpolation_search (list, key, low, high);
 
-  if (l[p] == key)
+  if (key == list[p])           // base case: key found
     return p;
 
-  if ((high - low) < 1)
+  // ERROR checks
+  if (low == p)                 // element not in the list
     return -1;
 
-  if (key < l[p])
-    search (l, key, low, p - 1);
+  if (low > high)               // limits out of order
+    return -2;
+
+  if (list[low] > list[high])   // list not sorted
+    return -3;
+
+  if (key < list[p])
+    search (list, key, low, p - 1);
   else
-    search (l, key, p + 1, high);
+    search (list, key, p + 1, high);
 }
 
-void print (int *l, int len)
+void
+print (int *list, int len)
 {
   int c;
   for (c = 0; c < len; c++) {
-    printf ("%d ", l[c]);
+    printf ("%d ", list[c]);
   }
   printf ("\n");
 }
 
 
-int main ()
+int
+main ()
 {
   int size = 10;
-  int l[10] = { 0, 1, 3, 5, 7, 11, 13, 17, 19, 23 };
+  int list[10] = { 0, 1, 3, 5, 7, 11, 13, 17, 19, 23 };
 
-  print (l, size);
-  printf ("pos of %d is %d\n", 3, search (l, 3, 0, size-1));
+  print (list, size);
+  printf ("pos of %d is %d\n", 3, search (list, 3, 0, size - 1));
+
+  printf ("pos of %d is %d\n", 18, search (list, 18, 0, size - 1));
 }
