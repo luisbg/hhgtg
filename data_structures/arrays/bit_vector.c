d0ec61e77b1ed0e74e3a62f2d6e47e21d4266afa
Luis de Bethencourt <luis@debethencourt.com>
2014-04-13 19:06:08 -0400

    arrays: bit vector unset

--- data_structures/arrays/bit_vector.c
+++ data_structures/arrays/bit_vector.c
@@ -7,51 +7,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define WORD_BITS 5
+#define MOD 0x1F
 
+/* initialize the bit vector */
 int *
 vector_init (int size)
 {
-  int i;
-  int * vector = (int *) malloc ((size >> 5) * sizeof (int));    // divide by 32
-
-  for (i = 0; i < (size >> 5); i++)
-    vector[i] = 0;
+  // array size is divided by 32 (since there are 32 bits per word)
+  int * vector = (int *) calloc (size >> WORD_BITS, sizeof (int));
 
   return vector;
 }
 
+/* get value at position in vector. check if the set has the number */
 int
 vector_get (int * bitvector, int pos)
 {
-  int word_num = (pos >> 5);   // divide by 32
-  int bit_num = (pos & 0x1F);  // mod by 32
+  int word_num = (pos >> WORD_BITS);   // divide by 32
+  int bit_num = (pos & MOD);  // mod by 32
 
-  return (bitvector[word_num] & (1 << bit_num)) != 0;
+  return bitvector[word_num] & (1 << bit_num);
 }
 
+/* set value at position in vector to True */
 void
 vector_set (int * bitvector, int pos)
 {
-  int word_num = (pos >> 5);   // divide by 32
-  int bit_num = (pos & 0x1F);  // mod by 32
+  int word_num = (pos >> WORD_BITS);   // divide by 32
+  int bit_num = (pos & MOD);  // mod by 32
 
   bitvector[word_num] |= (1 << bit_num);
+}
+
+/* set value at position in vector to False */
+void
+vector_unset (int * bitvector, int pos)
+{
+  int word_num = (pos >> WORD_BITS);   // divide by 32
+  int bit_num = (pos & MOD);  // mod by 32
+
+  bitvector[word_num] &= ~(1 << bit_num);
 }
 
 
 int main ()
 {
-  int i, r, set = 10, max = 30000;
+  int i, r,
+    set = 15,
+    max = 30000;
 
   int * vector = vector_init (max);
 
+  // set positions
   for (i = 0; i < set; i++) {
     r = rand () % max;
-    printf ("set number %d\n", r);
+    printf ("set number:  %5d\n", r);
     vector_set (vector, r);
+  }
 
-    printf ("%d is: %c\n", r, vector_get (vector, r)? '1': '0');
-    printf ("%d is: %c\n\n", r + 1, vector_get (vector, r + 1)? '1': '0');
+  // check what values the set has, print them all and unset some
+  printf ("\n");
+  for (i = 0; i < max; i++) {
+    if (vector_get (vector, i)) {
+        printf ("have in vector/set:  %5d\n", i);
+        if (rand () % 3)
+          vector_unset (vector, i);
+    }
+  }
+
+  // print all values in set
+  printf ("\nunset some\n\n");
+  for (i = 0; i < max; i++) {
+    if (vector_get (vector, i))
+        printf ("have in vector/set:  %5d\n", i);
   }
 
   return 0;
