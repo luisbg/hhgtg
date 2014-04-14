35fcc50937895523e1281736e53d70b7bf727853
Luis de Bethencourt <luis@debethencourt.com>
2014-04-14 16:27:40 -0400

    queue: code cleanup in queue

--- data_structures/queues/queue.c
+++ data_structures/queues/queue.c
@@ -1,3 +1,5 @@
+/* Queues by definition are FIFO */
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -13,9 +15,23 @@ typedef struct queue
   node *last;
 } queue;
 
+queue * init_queue ();
 void enqueue (queue * q, int data);
 int dequeue (queue * q);
 
+
+/* create and initialize the queue */
+queue *
+init_queue ()
+{
+  queue *q = (queue *) malloc (sizeof (queue));
+  q->head = NULL;
+  q->last = NULL;
+
+  return q;
+}
+
+/* add data to the tail of the queue */
 void
 enqueue (queue * q, int data)
 {
@@ -32,6 +48,7 @@ enqueue (queue * q, int data)
   }
 }
 
+/* get data from head of the queue */
 int
 dequeue (queue * q)
 {
@@ -48,40 +65,51 @@ dequeue (queue * q)
   return d;
 }
 
+/* delete the memory used by the queue */
+void
+free_queue (queue *q)
+{
+  while (q->head)
+    dequeue (q);
+
+  free (q);
+}
 
 int
 main ()
 {
-  queue q;
-  q.head = NULL;
-  q.last = NULL;
+  queue *q = init_queue();
 
   printf ("enqueue: %d\n", 0);
-  enqueue (&q, 0);
+  enqueue (q, 0);
   printf ("enqueue: %d\n", 1);
-  enqueue (&q, 1);
+  enqueue (q, 1);
   printf ("enqueue: %d\n", 2);
-  enqueue (&q, 2);
+  enqueue (q, 2);
   printf ("enqueue: %d\n", 3);
-  enqueue (&q, 3);
+  enqueue (q, 3);
 
   printf ("\n");
 
-  printf ("dequeue: %d\n", dequeue (&q));
-  printf ("dequeue: %d\n", dequeue (&q));
-  printf ("dequeue: %d\n", dequeue (&q));
-  printf ("dequeue: %d\n", dequeue (&q));
-  printf ("dequeue: %d\n", dequeue (&q));
+  printf ("dequeue: %d\n", dequeue (q));
+  printf ("dequeue: %d\n", dequeue (q));
+  printf ("dequeue: %d\n", dequeue (q));
+  printf ("dequeue: %d\n", dequeue (q));
+  printf ("dequeue: %d\n", dequeue (q));
 
   printf ("\n");
 
   printf ("enqueue: %d\n", 4);
-  enqueue (&q, 4);
+  enqueue (q, 4);
   printf ("enqueue: %d\n", 5);
-  enqueue (&q, 5);
+  enqueue (q, 5);
 
   printf ("\n");
 
-  printf ("dequeue: %d\n", dequeue (&q));
-  printf ("dequeue: %d\n", dequeue (&q));
+  printf ("dequeue: %d\n", dequeue (q));
+  printf ("dequeue: %d\n", dequeue (q));
+
+  free_queue (q);
+
+  return 0;
 }
