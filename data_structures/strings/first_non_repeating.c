4127823f549a13bcca9738f34c3c31e796cad561
Luis de Bethencourt <luis@debethencourt.com>
2014-04-14 13:25:51 -0400

    strings: more efficient first non-repeating for long strings

--- data_structures/strings/first_non_repeating.c
+++ data_structures/strings/first_non_repeating.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
-/* Find the frist non-repeating character */
+typedef struct {
+  bool seen;
+  bool repeated;
+  int pos;
+} repeat_counter_t;
+
+
+/* Find the first non-repeating character */
 char
 first_non_repeat (char *in)
 {
-  int *charcount = (int *) calloc (2 << sizeof (char), sizeof (int));
+  int *charcount = (int *) calloc (sizeof (char) << 8, sizeof (int));
   int len;
   int i;
 
+  // count the occurrences of each char
   for (i = 0; in[i]; i++) {
     charcount[in[i]]++;
   }
 
+
+  // find the first char in the string with one ocurrence
   len = i - 1;
   for (i = 0; i < len; i++)
     if (charcount[in[i]] == 1) {
@@ -25,12 +36,51 @@ first_non_repeat (char *in)
   return '\0';
 }
 
+/* Find the first non-repeating character for long strings */
+char
+first_non_repeat_long (char *in)
+{
+  const int char_range = sizeof (char) << 8;
+  repeat_counter_t *rep_count = (repeat_counter_t *) calloc (char_range,
+      sizeof (repeat_counter_t));
+  int lowest_pos = char_range;
+  int i;
+
+  // run through character type range checking if seen and repeated
+  for (i = 0; in[i]; i++) {
+    if (!rep_count[in[i]].repeated) {
+      if (!rep_count[in[i]].seen) {
+        rep_count[in[i]].seen = true;
+        rep_count[in[i]].pos = i;
+      } else
+        rep_count[in[i]].repeated = true;
+    }
+  }
+
+  // go through range finding the unrepeated character with the lowest position
+  for (i = 0; i < char_range; i++) {
+    if (rep_count[i].seen && !rep_count[i].repeated &&
+      rep_count[i].pos < lowest_pos) {
+      lowest_pos = rep_count[i].pos;
+    }
+  }
+
+  free (rep_count);
+  if (lowest_pos != char_range)
+    return in[lowest_pos];
+  else
+    return '\0';
+}
+
 int
 main ()
 {
-  char *str = "this is the best";
+  char *str = "this is either the worst or the best";
   printf ("the first non-repeating character of: %s\nis: %c\n",
-      str, first_non_repeat ("this is the best"));
+      str, first_non_repeat (str));
 
+  str = "this is a much longer string that might be longer than two hundred fifty six characters";
+  printf ("the first non-repeating character of: %s\nis: %c\n",
+      str, first_non_repeat_long (str));
   return 0;
 }
