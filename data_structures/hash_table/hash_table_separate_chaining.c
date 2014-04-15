4a1d11dc05c136df328b506a501016039535d4fb
Luis de Bethencourt <luis@debethencourt.com>
2014-04-15 16:17:47 -0400

    hash table: clean up code for separate chaining

--- data_structures/hash_table/hash_table_separate_chaining.c
+++ data_structures/hash_table/hash_table_separate_chaining.c
@@ -3,54 +3,94 @@
 #include <string.h>
 
 typedef struct linked_list
-{                               /* has collision by separate chaining */
-  char key[16];                 /* with head records in the bucket    */
-  char word[32];                /* array                              */
+{
+  char *key;
+  char *word;
   struct linked_list *next;
-} linked_list;
+} linked_list_t;
+
+typedef struct hash_table
+{
+  int size;                     /* has collision by separate chaining */
+  int elements;                 /* with head records in the bucket array */
+  linked_list_t *buckets;
+} hash_table_t;
 
 typedef enum
 { FALSE, TRUE } bool;
 
 
+/* simple hash to predict collisions */
 int
 hash (char *key)
 {
   return key[0] % 16;
 }
 
-void
-insert_list (linked_list * l, char key[], char word[])
+/* initialize hash table */
+hash_table_t *
+init_hash_table (int size)
 {
+  int i;
+  hash_table_t *t = (hash_table_t *) malloc (sizeof (hash_table_t));
+
+  t->size = size;
+  t->elements = 0;
+  t->buckets = (linked_list_t *) malloc (size * sizeof (linked_list_t));
+
+  for (i = 0; i < size; i++) {
+    t->buckets[i].key = (char *) malloc (16 * sizeof (char));
+    strcpy(t->buckets[i].key, "");
+    t->buckets[i].word = (char *) malloc (32 * sizeof (char));
+  }
+
+  return t;
+}
+
+/* insert (key, word) into the linked list */
+void
+insert_list (linked_list_t *l, char key[], char word[])
+{
+  printf ("insert %s->%s\n", key, word);
+
+  linked_list_t * run;
+
+  // if first element in list, insert in head
   if (strcmp (l->key, "") == 0) {
     strcpy (l->key, key);
     strcpy (l->word, word);
     l->next = NULL;
   } else {
-    while (l->next) {
-      l = l->next;
+    run = l;
+    // if not first, create a new node and link from last
+    while (run->next) {
+      run = run->next;
     }
 
-    linked_list *new_node;      /* temporary pointer */
-    new_node = malloc (sizeof (linked_list));
+    linked_list_t *new_node;
+    new_node = malloc (sizeof (linked_list_t));
+
+    new_node->key = (char *) malloc (16 * sizeof (char));
+    new_node->word = (char *) malloc (32 * sizeof (char));
 
     strcpy (new_node->key, key);
     strcpy (new_node->word, word);
     new_node->next = NULL;
 
-    l->next = new_node;
+    run->next = new_node;
   }
-
-  printf ("inserted %s->%s\n", key, word);
 }
 
+/* get word matching the key in the linked list */
 char *
-get_list (linked_list l, char *key)
+get_list (linked_list_t *l, char *key)
 {
-  if (strcmp (l.key, key) == 0) {
-    return l.word;
+  linked_list_t *run;
+
+  if (strcmp (l->key, key) == 0) {
+    return l->word;
   } else {
-    linked_list *run = l.next;
+    run = l->next;
     while (run) {
       if (strcmp (run->key, key) == 0) {
         return run->word;
@@ -63,30 +103,33 @@ get_list (linked_list l, char *key)
   return "";
 }
 
-bool
-insert_hash_table (linked_list * t, char key[], char word[])
+/* insert (key, word) into the hash table */
+void
+insert_hash_table (hash_table_t * t, char key[], char word[])
 {
   int h = hash (key);
-  insert_list (&t[h], key, word);
+  insert_list (&(t->buckets[h]), key, word);
 }
 
+/* the the word matching the key from the table */
 char *
-get_from_table (linked_list * t, char key[])
+get_from_table (hash_table_t * t, char key[])
 {
   int h = hash (key);
-  return get_list (t[h], key);
+  return get_list (&(t->buckets[h]), key);
 }
+
 
 int
 main ()
 {
-  linked_list table[32] = { 0 };
+  hash_table_t *table = init_hash_table (32);
 
   printf ("created table\n");
 
   insert_hash_table (table, "first", "work it");
   insert_hash_table (table, "second", "make it");
-  /* skip third for testing getting onstored keys */
+  /* skip third for testing getting stored keys */
   insert_hash_table (table, "fourth", "make us");
   insert_hash_table (table, "fifth", "harder");
   insert_hash_table (table, "sixth", "better");
@@ -95,23 +138,27 @@ main ()
   insert_hash_table (table, "ninth", "more than");
   insert_hash_table (table, "tenth", "hour");
 
+  printf (" \n");
+
   char *k = "first";
   printf ("the key: %s -> has word: %s \n", k, get_from_table (table, k));
   k = "second";
   printf ("the key: %s -> has word: %s \n", k, get_from_table (table, k));
   k = "third";
-  if (get_from_table (table, k) == "")
+  if (strcmp (get_from_table (table, k), ""))
     printf ("the key: %s -> isn't stored\n", k);
   k = "sixth";                  /* sixth collides with second */
   printf ("the key: %s -> has word: %s \n", k, get_from_table (table, k));
   k = "seventh";                /* seventh collides with second and sixth */
-  printf ("the key: %s -> has word: %s \n", k, get_from_table (table, k));
+  printf ("the key: %s -> has word: %s \n \n", k, get_from_table (table, k));
 
   int c;
   for (c = 0; c < 32; c++) {
-    if (strcmp (table[c].key, "") != 0) {
-      printf ("table head records %d: %s->%s\n", c, table[c].key,
-          table[c].word);
+    if (strcmp (table->buckets[c].key, "") != 0) {
+      printf ("table head records %d: %s->%s\n", c, table->buckets[c].key,
+          table->buckets[c].word);
     }
   }
+
+  return 0;
 }
