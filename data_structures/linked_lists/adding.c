4de8c2e84b29e44b51e415d1c6af3082cb595284
Luis de Bethencourt <luis@debethencourt.com>
2014-04-16 23:24:24 -0400

    linked list: adding directly with lists

--- data_structures/linked_lists/adding.c
+++ data_structures/linked_lists/adding.c
@@ -78,7 +78,7 @@ number_to_list (node ** l, int n)
   }
 }
 
-/* Add the two lists */
+/* Add the two lists converting them to numbers and back */
 void
 add (node * a, node * b, node ** sum)
 {
@@ -89,13 +89,90 @@ add (node * a, node * b, node ** sum)
   number_to_list (sum, sum_a + sum_b);
 }
 
+/* Sum two linked lists into one */
+void
+add_with_lists (node * a, node * b, node ** sum)
+{
+  node *runa, *runb;
+  int lena = 0,
+    lenb = 0,
+    carry_over = 0,
+    tmp, i;
+
+  runa = a;
+  runb = b;
+  while (runa->next) {    // get number of digits in a
+    runa = runa->next;
+    lena++;
+  }
+
+  while (runb->next) {    // get number of digits in b
+    runb = runb->next;
+    lenb++;
+  }
+
+  while (lena >= 0 && lenb >= 0) {
+    tmp = runa->d + runb->d;    // add elements from the tail back from both lists
+    if (tmp < 10) {                         // if sum above 10, carry over
+      prepend (sum, tmp + carry_over);
+      carry_over = 0;
+    } else {
+      prepend (sum, (tmp - 10) + carry_over);
+      carry_over = 1;
+    }
+
+    i = --lena;
+    runa = a;
+    while (i > 0) {           // decrement len to go to cycle to previous node
+      runa = runa->next;
+      i--;
+    }
+
+    i = --lenb;
+    runb = b;
+    while (i > 0) {
+      runb = runb->next;
+      i--;
+    }
+  }
+
+  runa = a;                   // one of the lists might still have more digits
+  while (lena >= 0) {         // prepend them along
+    i = --lena;
+    runa = a;
+    while (i > 0) {
+      runa = runa->next;
+      i--;
+    }
+    prepend (sum, runa->d + carry_over);
+    carry_over = 0;
+  }
+
+  runb = b;
+  while (lenb >= 0) {
+    i = --lenb;
+    runb = b;
+    while (i > 0) {
+      runb = runb->next;
+      i--;
+    }
+    prepend (sum, runb->d + carry_over);
+    carry_over = 0;
+  }
+
+  if (carry_over)           // we might still have a carry over if the two
+    prepend (sum, 1);       // lists have same amount of digits in MSB add > 10
+}
+
 
 int
 main ()
 {
   node *a = NULL;
   node *b = NULL;
+  node *c = NULL;
   node *sum = NULL;
+  node *second_sum = NULL;
 
   prepend (&a, 6);
   prepend (&a, 1);
@@ -110,6 +187,19 @@ main ()
 
   add (a, b, &sum);
   travel (sum);
+
+  printf ("\n");
+
+  prepend (&c, 3);
+  prepend (&c, 5);
+  prepend (&c, 8);
+
+  travel (b);
+  travel (c);
+
+  // todo: add function to free memory to reuse first sum list
+  add_with_lists (b, c, &second_sum);
+  travel (second_sum);
 
   return 0;
 }
