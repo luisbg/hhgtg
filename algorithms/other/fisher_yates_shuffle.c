bc21ce0019561aaeb7b531a5b6a8a377fa4471a3
Luis de Bethencourt <luisbg@osg.samsung.com>
2015-11-14 14:45:39 +0000

    other: add inside out Fisher Yates Shuffle

--- algorithms/other/fisher_yates_shuffle.c
+++ algorithms/other/fisher_yates_shuffle.c
@@ -17,16 +17,30 @@ void shuffle (int size, int set[size]) {
   }
 }
 
+/* simultaneously initialize and shuffle the set */
+void inside_out (int size, int set[size]) {
+  int i, j;
+
+  for (i = 0; i < size; i++) {
+    j = rand() % (i + 1);
+    if (j != i)
+      swap (&set[i], &set[j]);
+
+    /* value passed to set[j] could come from an external source.next */
+    set[j] = i;
+  }
+}
 
 int main () {
   int size = 16;
   int set[16];
+  int io[16];
   int i;
 
   /* use time as seed for pseud-randomness */
   srand(time(NULL));
 
-  /* new sorted deck */
+  /* new sorted list, only set and not io */
   for (i = 0; i < size; i++)
     set[i] = i + 1;
 
@@ -35,11 +49,18 @@ int main () {
     printf (" %2d", set[i]);
   printf ("\n");
 
-  shuffle (size, set);
-
   printf ("fisher yates shuffled set: \t");
+
+  shuffle (size, set);
   for (i = 0; i < size; i++)
     printf (" %2d", set[i]);
+  printf ("\n");
+
+  printf ("inside out shuffled set: \t");
+
+  inside_out (size, io);
+  for (i = 0; i < size; i++)
+    printf (" %2d", io[i]);
   printf ("\n");
 
   return 0;
