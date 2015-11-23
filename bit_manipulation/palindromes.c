28260e97e8c0899cef97404e1e9febc90b35eb5f
Luis de Bethencourt <luisbg@osg.samsung.com>
2015-11-23 14:58:10 +0000

    bit manipulation: add explanation to palindromes.c

--- bit_manipulation/palindromes.c
+++ bit_manipulation/palindromes.c
@@ -1,4 +1,7 @@
-/* Inspired by Project Euler problem 36 */
+/* Inspired by Project Euler problem 36
+ *
+ * Find the sum of all numbers less than one million, which are palindromic in
+ * base 10 and base 2 */
 
 #include <stdlib.h>
 #include <stdio.h>
