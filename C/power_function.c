70a5aaec9046d258d92722118a269b34f12db1ff
Luis de Bethencourt <luisbg@osg.samsung.com>
2015-11-24 12:15:25 +0000

    C: accept negative exponents

--- C/power_function.c
+++ C/power_function.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int power (int m, int n);
+
+double power (int m, int n);
 
 /* test power function */
 int
 main ()
 {
   int i;
-  for (i = 0; i < 10; ++i)
-    printf ("%d %d %d\n", i, power (2, i), power (-3, i));
+  for (i = -5; i < 10; ++i)
+    printf ("%d: 2^x= %.2f   -3^x= %.2f\n", i, power (2, i), power (-3, i));
 
   return 0;
 }
 
-/* power: raise base to n-th power; n >= 0 */
+/* power: raise base to n-th power; n >= 0 || n < 0 */
 // since n is passed by value we can use it without changing the original
-int
+double
 power (int base, int n)
 {
-  int p;
-  for (p = 1; n > 0; --n)
-    p = p * base;
+  double p = 1;
+  int i;
+
+  for (i = 0; i < abs(n); i++)
+    p *= base;
+
+  if (n < 0)
+    return 1 / p;
+
   return p;
 }
