0913076a54d00c162a8ccb78c837aae698efe728
Luis de Bethencourt <luisbg@osg.samsung.com>
2017-02-26 18:51:10 +0000

    linked list: reuse existing nodes when merging
    We can reuse the existing nodes from the two lists, when splicing them
    into a new one. This brings the space complexity down to O(1).

--- data_structures/linked_lists/merge_sorted.c
+++ data_structures/linked_lists/merge_sorted.c
@@ -2,6 +2,9 @@
  * Merge two sorted lists A and B as one linked list
  */
 
+// TODO: if we keep track of tail of new list, we avoid traversing it
+// for each Append()
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,14 +20,23 @@ typedef enum
   FALSE, TRUE
 } bool;
 
+Node *
+RemoveHead (Node ** l)
+{
+  Node *tmp;
+
+  if (*l) {                     // not an empty list
+    tmp = *l;
+    *l = tmp->next;             // list points to the second
+  }
+
+  return tmp;
+}
 
 /* Append data to tail of list */
 static void
-Append (Node ** head, int data)
+Append (Node ** head, Node * new_node)
 {
-  // printf ("Append: %d\n", data);
-  Node *new_node = (Node *) malloc (sizeof (Node));
-  new_node->data = data;
   new_node->next = NULL;
 
   Node *run = *head;
@@ -53,28 +65,36 @@ Print (Node * head)
 Node *
 MergeLists (Node * headA, Node * headB)
 {
+  Node *head = NULL;
   Node *tmp = NULL;
+
+  if (!headA || !headB)
+    return NULL;
 
   while (headA && headB) {      // run through both lists until one is empty
     if (headA->data < headB->data) {    // appending the smallest new head
-      Append (&tmp, headA->data);
-      headA = headA->next;
+      tmp = RemoveHead (&headA);
+      headA = tmp->next;
+      Append (&head, tmp);
     } else {
-      Append (&tmp, headB->data);
-      headB = headB->next;
+      tmp = RemoveHead (&headB);
+      headB = tmp->next;
+      Append (&head, tmp);
     }
   }
 
   while (headA) {               // one of the two lists isn't empty yet
-    Append (&tmp, headA->data);
-    headA = headA->next;
+    tmp = RemoveHead (&headA);
+    headA = tmp->next;
+    Append (&head, tmp);
   }
   while (headB) {
-    Append (&tmp, headB->data);
-    headB = headB->next;
+    tmp = RemoveHead (&headB);
+    headB = tmp->next;
+    Append (&head, tmp);
   }
 
-  return tmp;
+  return head;
 }
 
 
@@ -84,22 +104,22 @@ main ()
   Node *A = NULL;
   Node *B = NULL;
 
-  Append (&A, 0);
-  Append (&A, 3);
-  Append (&A, 5);
-  Append (&A, 6);
-  Append (&A, 7);
-  Append (&A, 8);
+  for (int i = 0; i < 10; i = i + 2) {
+    Node *tmp = (Node *) malloc (sizeof (Node));
+    tmp->data = i;
+    tmp->next = NULL;
+    Append (&A, tmp);
+  }
 
-  Append (&B, 1);
-  Append (&B, 2);
-  Append (&B, 3);
-  Append (&B, 4);
-  Append (&B, 5);
-  Append (&B, 6);
-  Append (&B, 7);
-  Append (&B, 8);
-  Append (&B, 9);
+  for (int i = 1; i < 15; i++) {
+    if (i % 2 == 0)
+      continue;
+
+    Node *tmp = (Node *) malloc (sizeof (Node));
+    tmp->data = i;
+    tmp->next = NULL;
+    Append (&B, tmp);
+  }
 
   Print (A);
   Print (B);
