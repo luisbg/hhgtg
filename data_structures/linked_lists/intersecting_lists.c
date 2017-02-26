0f8fe8001eb26affccd880db1fd150da55e0354f
Luis de Bethencourt <luisbg@osg.samsung.com>
2017-02-26 15:45:36 +0000

    linked list: intersection of linked lists

---
+++ data_structures/linked_lists/intersecting_lists.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+// Definition for singly-linked list.
+typedef struct ListNode
+{
+  int val;
+  struct ListNode *next;
+} ListNode;
+
+bool
+check_if_lists_intersect (ListNode * headA, ListNode * headB)
+{
+  if (!headA || !headB)
+    return false;
+
+  while (headA->next)
+    headA = headA->next;
+
+  while (headB->next)
+    headB = headB->next;
+
+  return headA->val == headB->val;
+}
+
+int
+get_list_length (ListNode * head)
+{
+  int len = 0;
+
+  while (head) {
+    len++;
+    head = head->next;
+  }
+
+  return len;
+}
+
+struct ListNode *
+get_intersecting_node (ListNode * headA, ListNode * headB)
+{
+  ListNode *run_a = headA;
+  ListNode *run_b = headB;
+
+  if (check_if_lists_intersect (headA, headB) == false)
+    return NULL;
+
+  int len_a = get_list_length (headA);
+  int len_b = get_list_length (headB);
+
+  while (len_a != len_b) {
+    if (len_a > len_b) {
+      run_a = run_a->next;
+      len_a--;
+    } else if (len_a < len_b) {
+      run_b = run_b->next;
+      len_b--;
+    }
+  }
+
+  while (run_a != run_b) {
+    run_a = run_a->next;
+    run_b = run_b->next;
+  }
+
+  return run_a;
+}
+
+bool
+insert_append (ListNode ** l, int val)
+{
+  ListNode *run;
+  ListNode *new_node = (ListNode *) malloc (sizeof (ListNode));
+  if (!new_node)
+    return false;
+
+  new_node->val = val;
+  new_node->next = NULL;
+
+  if (*l) {
+    run = *l;
+    while (run->next)           // go to last node
+      run = run->next;
+
+    run->next = new_node;       // append new_node as next of last
+  } else {
+    *l = new_node;
+  }
+
+  return true;
+}
+
+bool
+create_intersecting_node (ListNode ** list_a, ListNode ** list_b, int val)
+{
+  ListNode *run;
+  ListNode *new_node = (ListNode *) malloc (sizeof (ListNode));
+  if (!new_node)
+    return false;
+
+  new_node->val = val;
+  new_node->next = NULL;
+
+  // a
+  if (*list_a) {
+    run = *list_a;
+    while (run->next)
+      run = run->next;
+
+    run->next = new_node;
+  } else {
+    *list_a = new_node;
+  }
+
+  // b
+  if (*list_b) {
+    run = *list_b;
+    while (run->next)
+      run = run->next;
+
+    run->next = new_node;
+  } else {
+    *list_b = new_node;
+  }
+
+  return true;
+}
+
+int
+main ()
+{
+  ListNode *list_a = NULL;
+  ListNode *list_b = NULL;
+  ListNode *intersect;
+
+  printf ("created two empty lists\n");
+  intersect = get_intersecting_node (list_a, list_b);
+  if (intersect)
+    printf ("intersection at: %d\n\n", intersect->val);
+  else
+    printf ("no intersection node\n\n");
+
+  printf ("appending 0..2 in first list, and 5..10 in the second\n");
+  for (int i = 0; i < 3; i++)
+    insert_append (&list_a, i);
+  for (int i = 5; i < 11; i++)
+    insert_append (&list_b, i);
+
+  intersect = get_intersecting_node (list_a, list_b);
+  if (intersect)
+    printf ("intersection at: %d\n\n", intersect->val);
+  else
+    printf ("no intersection node\n\n");
+
+  printf ("creating an insterting node with value 42\n");
+  create_intersecting_node (&list_a, &list_b, 42);
+
+  intersect = get_intersecting_node (list_a, list_b);
+  if (intersect)
+    printf ("intersection at: %d\n\n", intersect->val);
+  else
+    printf ("no intersection node\n\n");
+
+  printf ("appending 21..23 to the first list\n");
+  for (int i = 21; i < 24; i++)
+    insert_append (&list_a, i);
+
+  intersect = get_intersecting_node (list_a, list_b);
+  if (intersect)
+    printf ("intersection at: %d\n", intersect->val);
+  else
+    printf ("no intersection node\n");
+
+  return 0;
+}
