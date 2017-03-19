02153a3ec6cfc36ab3ad1597874ff4ef446f9d20
Luis de Bethencourt <luisbg@osg.samsung.com>
2017-03-19 14:19:22 +0000

    C++: add hashmaps example

---
+++ C++/hashmaps.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int
+main ()
+{
+  unordered_map < string, string > capitals;
+  capitals = { {"Canada", "Ottawa"},
+	  {"Germany", "Berlin"},
+	  {"Ireland", "Dublin"},
+	  {"Spain", "Madrid"},
+	  {"UK", "London"},
+	  {"USA", "Washington DC"} };
+
+  cout << "capitals has size: " << capitals.size () << endl;
+  cout << endl << "capitals contains:" << endl;
+  for (auto it = capitals.begin (); it != capitals.end (); ++it)
+    std::cout << " " << it->first << ":" << it->second << endl;
+  cout << endl;
+
+  std::cout << "capitals's buckets contain:\n";
+  for (unsigned i = 0; i < capitals.bucket_count (); ++i) {
+    std::cout << "bucket #" << i << " contains:";
+    for (auto local_it = capitals.begin (i); local_it != capitals.end (i);
+        ++local_it)
+      std::cout << " - " << local_it->first << ":" << local_it->second;
+    std::cout << std::endl;
+  }
+
+  cout << endl << "Capital of Spain: "  << capitals["Spain"] << endl;
+
+  cout << endl << "erase Ireland" << endl;
+  capitals.erase ("Ireland");
+  cout << "insert South Korea" << endl;
+  capitals.insert ( {"South Korea", "Seoul"} );
+
+  cout << endl << "capitals now contains:" << endl;
+  for (auto it = capitals.begin (); it != capitals.end (); ++it)
+    std::cout << " " << it->first << ":" << it->second << endl;
+  cout << endl;
+
+  return 0;
+}
