ff2ff2ab6412713833cab7c9203093d1f33f68f0
Luis de Bethencourt <luisbg@osg.samsung.com>
2017-03-28 22:01:36 +0100

    strings: clean reverse words

--- data_structures/strings/reverse_words.c
+++ data_structures/strings/reverse_words.c
@@ -6,96 +6,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef enum
 {
   FALSE, TRUE
 } bool;
 
-bool reverse_order_words_elegant (char **phrase);
-bool reverse_order_words_general (char **phrase);
-static void copy_str_split (char *phrase, char **out, int c, int d, int start);
-static void reverse_string (char **phrase, int start, int end);
+void reverse_order_words (char *phrase);
+static void reverse_string (char *phrase, int start, int end);
 
-bool
-reverse_order_words_elegant (char **phrase)
+
+void
+reverse_order_words (char *phrase)
 {
-  int c = 0, end = 0, size;
-
-  while ((*phrase)[c] != '\0')
-    c++;
-  size = c;
-  c = 0;
-
-  reverse_string (phrase, c, size - 1); // revese the entire string
-
-  while (end < size) {
-    if ((*phrase)[end] != ' ') {        // start of word
-      c = end;
-      while (end < size && (*phrase)[end] != ' ')
-        end++;
-
-      end--;                    // go back to end of word and not the space
-      reverse_string (phrase, c, end);  // reverse word back to original
-    }
-
-    end++;                      // advance to next token
+  reverse_string (phrase, 0, strlen (phrase) - 1);
+  for (int i = 0, j = 0; i < strlen (phrase); i = j + 1) {
+    for (j = i; j < strlen (phrase) && phrase[j] != ' '; j++);
+    reverse_string (phrase, i, j - 1);
   }
-
-  return TRUE;
-}
-
-bool
-reverse_order_words_general (char **phrase)
-{
-  int c, end, n_start, size;
-
-  c = 0;
-  while ((*phrase)[c] != '\0')
-    c++;
-  size = c;
-  end = size - 1;
-  n_start = 0;
-
-  char *tmp = (char *) malloc ((size + 1) * sizeof (char));
-
-  for (c = size - 1; c >= 0; c--) {
-    if ((*phrase)[c] == ' ') {
-      copy_str_split (*phrase, &tmp, c + 1, end, n_start);
-      n_start += (end - c) + 1;
-      end = c - 1;
-    }
-  }
-
-  copy_str_split (*phrase, &tmp, 0, end, n_start);
-  tmp[size] = '\0';
-
-  *phrase = tmp;
-
-  return TRUE;
 }
 
 static void
-reverse_string (char **phrase, int start, int end)
+reverse_string (char *phrase, int start, int end)
 {
   char tmp;
   while (end > start) {
-    tmp = (*phrase)[start];
-    (*phrase)[start] = (*phrase)[end];
-    (*phrase)[end] = tmp;
+    tmp = phrase[start];
+    phrase[start] = phrase[end];
+    phrase[end] = tmp;
 
     start++;
     end--;
   }
-}
-
-static void
-copy_str_split (char *phrase, char **out, int c, int end, int pos)
-{
-  while (c <= end) {
-    (*out)[pos++] = phrase[c++];
-  }
-  (*out)[pos] = ' ';
 }
 
 
@@ -104,13 +47,10 @@ main ()
 {
   char *phrase = (char *) malloc (30 * sizeof (char));
   char *tmp = "Do or do not, there is no try.";
-  int c;
-  for (c = 0; tmp[c] != '\0'; c++) {
-    phrase[c] = tmp[c];
-  }
+  strcpy (phrase, tmp);
 
   printf ("original: %s\n", phrase);
-  reverse_order_words_elegant (&phrase);
+  reverse_order_words (phrase);
   printf ("reversed: %s\n", phrase);
 
   return 0;
